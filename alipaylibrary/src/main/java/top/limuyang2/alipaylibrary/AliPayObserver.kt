c9279871549968956242946347a0ac3b69f0ce71
kiwilss <m15205622327@163.com>
2018-10-17 08:56:15 +0800

    Initial commit

---
+++ alipaylibrary/src/main/java/top/limuyang2/alipaylibrary/AliPayObserver.kt
@@ -0,0 +1,12 @@
+package top.limuyang2.alipaylibrary
+
+import top.limuyang2.basepaylibrary.BasePayObserver
+
+/**
+ * @author limuyang
+ * @date 2018/8/12
+ * @class describe
+ */
+interface AliPayObserver : BasePayObserver
+
+abstract class JavaAliPayObserver : AliPayObserver
